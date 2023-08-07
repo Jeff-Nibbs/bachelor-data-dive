a1275065326c358b56da903a2800ce7c07ec1f04
Jeffrey Nibbs <djcountry100@gmail.com>
2023-08-07 13:39:25 -0700

    first commit

---
+++ work/navbar-training/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <div className="h-14 w-14 rounded-full bg-sky-400 fixed top-5 right-5 flex items-center justify-center">
+        <div className="hamburger"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
